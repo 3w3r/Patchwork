545122a6ff79e15540111f57b6a7a44cf74bd0d6
Mark Ewer <MEwer@paylocity.com>
2025-01-16 15:14:15 -0600

    Added unit test and sort parser (#15)

---
+++ Spike_Solutions/Filter-Builder-Spike/Patchwork.Filters.Tests/SortTokenizer.Tests.cs
@@ -0,0 +1,91 @@
+using Patchwork.Sort;
+
+namespace Patchwork.Filters.Tests;
+
+public class SortLexerTests
+{
+  [Fact]
+  public void Parse_ReturnsEmptyString_WhenNoExpressionProvided()
+  {
+    // Arrange
+    var tokens = new List<SortToken>(); // No tokens provided
+    var parser = new MsSortTokenParser(tokens);
+
+    // Act
+    var result = parser.Parse();
+
+    // Assert
+    Assert.Empty(tokens);
+  }
+
+  [Theory]
+  [InlineData("firstname:asc,lastname:desc", "[firstname], [lastname] DESC", 2, 1, 1)]
+  [InlineData("firstname:asc       ,lastname    :desc       ", "[firstname], [lastname] DESC", 2, 1, 1)]
+  [InlineData("       firstname:asc,lastname         :desc", "[firstname], [lastname] DESC", 2, 1, 1)]
+  [InlineData("A,B,C,D,E,F", "[A], [B], [C], [D], [E], [F]", 6, 6, 0)]
+  [InlineData("A,B:desc,C,D:desc,E,F", "[A], [B] DESC, [C], [D] DESC, [E], [F]", 6, 4, 2)]
+  [InlineData("A,B:asc,C,D:asc,E:desc,F", "[A], [B], [C], [D], [E] DESC, [F]", 6, 5, 1)]
+  [InlineData("A,But_I_Think_This_Column_Name_Is_Really_Long:desc", "[A], [But_I_Think_This_Column_Name_Is_Really_Long] DESC", 2, 1, 1)]
+  public void Parse_ReturnsOrderByClause_MsSqlSuccessCases(string sort, string expected, int count, int ascending, int descending)
+  {
+    // Arrange
+    var lex = new SortLexer(sort);
+
+    // Act
+    var tokens = lex.Tokenize();
+    var parser = new MsSortTokenParser(tokens);
+    var result = parser.Parse();
+
+    // Assert
+    Assert.Equal(count, tokens.Count);
+    Assert.Equal(expected, result);
+    Assert.Equal(ascending, tokens.Where(t => t.Direction == SortDirection.Ascending).Count());
+    Assert.Equal(descending, tokens.Where(t => t.Direction == SortDirection.Descending).Count());
+  }
+
+  [Theory]
+  [InlineData("firstName:asc,lastName:desc", "firstname, lastname desc", 2, 1, 1)]
+  [InlineData("firstName:asc       ,lastname    :desc       ", "firstname, lastname desc", 2, 1, 1)]
+  [InlineData("       firstname:asc,lastname         :desc", "firstname, lastname desc", 2, 1, 1)]
+  [InlineData("A,B,C,D,E,F", "a, b, c, d, e, f", 6, 6, 0)]
+  [InlineData("A,B:desc,C,D:desc,E,F", "a, b desc, c, d desc, e, f", 6, 4, 2)]
+  [InlineData("A,B:asc,C,D:asc,E:desc,F", "a, b, c, d, e desc, f", 6, 5, 1)]
+  [InlineData("A,But_I_Think_This_Column_Name_Is_Really_Long:desc", "a, but_i_think_this_column_name_is_really_long desc", 2, 1, 1)]
+  public void Parse_ReturnsOrderByClause_PostgreSqlSuccessCases(string sort, string expected, int count, int ascending, int descending)
+  {
+    // Arrange
+    var lex = new SortLexer(sort);
+
+    // Act
+    var tokens = lex.Tokenize();
+    var parser = new PostgreSqlSortTokenParser(tokens);
+    var result = parser.Parse();
+
+    // Assert
+    Assert.Equal(count, tokens.Count);
+    Assert.Equal(expected, result);
+    Assert.Equal(ascending, tokens.Where(t => t.Direction == SortDirection.Ascending).Count());
+    Assert.Equal(descending, tokens.Where(t => t.Direction == SortDirection.Descending).Count());
+  }
+
+  [Theory]
+  [InlineData("firstname:asc,la%name:desc", "Invalid sort expression: la%name:desc is not a valid field name")]
+  [InlineData("^A,B,C,D,E,F:desc", "Invalid sort expression: ^A is not a valid field name")]
+  [InlineData("A,B,C,D,E,F:dasc", "Invalid sort expression: dasc is not valid sort order")]
+  [InlineData("A,B,C,D,E,F:goat roper", "Invalid sort expression: 'goat roper' is not valid sort order")]
+  [InlineData("A B C", "Invalid sort expression: Invalid column names")]
+  [InlineData("A,Bad;character", "Invalid sort expression: Invalid column names")]
+  public void Parse_ThrowsArgumentException_ForInvalidSortExpression(string sort, string errorMessage)
+  {
+    // Arrange
+    var lex = new SortLexer(sort);
+
+    var ex = Assert.ThrowsAny<ArgumentException>(() =>
+    {
+      var tokens = lex.Tokenize();
+      var parser = new MsSortTokenParser(tokens);
+      var result = parser.Parse();
+    });
+    if (ex == null) throw new Exception(errorMessage);
+  }
+}
---
+++ Spike_Solutions/Filter-Builder-Spike/Patchwork.Sort/Class1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patchwork.Sort;
+
+public class SortLexer
+{
+  private string _input;
+
+  public SortLexer(string input)
+  {
+    _input = input;
+  }
+
+  public List<SortToken> Tokenize()
+  {
+    var tokens = new List<SortToken>();
+    var split = _input.Trim().Split(',');
+    foreach (var token in split)
+    {
+      var tokenValue = token.Trim();
+      var t = MakeValidToken(tokenValue);
+      tokens.Add(t);
+    }
+
+    return tokens;
+  }
+  public SortToken MakeValidToken(string columnSort)
+  {
+    if (!char.IsLetter(columnSort.First()))
+      throw new ArgumentException("Column name must begin with a letter.");
+
+    if (columnSort.Contains(':'))
+      return MakeSortedColumnToken(columnSort);
+    else return MakeAscendingColumnToken(columnSort);
+  }
+
+  private SortToken MakeAscendingColumnToken(string columnSort)
+  {
+    ValidateColumnName(columnSort);
+    return new SortToken(columnSort, SortDirection.Ascending);
+  }
+
+  private static void ValidateColumnName(string columnSort)
+  {
+    foreach (var c in columnSort.ToArray())
+    {
+      if (char.IsLetter(c))
+        continue;
+      if (char.IsDigit(c))
+        continue;
+      if (c == '_')
+        continue;
+      throw new ArgumentException($"Invalid character found in column name: {columnSort}");
+    }
+  }
+
+  private SortToken MakeSortedColumnToken(string columnSort)
+  {
+    var split = columnSort.Split(':');
+    if (split.Length != 2)
+      throw new ArgumentException("Invalid sort direction. Use only column names and 'asc' or 'desc'.");
+    var name = split[0].Trim();
+    ValidateColumnName(name);
+
+    var direction = SortDirection.Ascending;
+    switch (split[1].Trim().ToLower())
+    {
+      case "asc":
+        break;
+      case "desc":
+        direction = SortDirection.Descending;
+        break;
+      default:
+        throw new ArgumentException($"Invalid sort order: {split[1].Trim()} is not asc or desc.");
+    }
+
+    return new SortToken(name, direction);
+  }
+}
+
+public class SortToken
+{
+  public SortToken(string column, SortDirection direction)
+  {
+    Column = column;
+    Direction = direction;
+  }
+  public string Column { get; set; } = string.Empty;
+  public SortDirection Direction { get; set; } = SortDirection.Ascending;
+}
+
+public enum SortDirection
+{
+  Ascending, Descending
+}
+
+public class PostgreSqlSortTokenParser
+{
+  private List<SortToken> _tokens;
+  public PostgreSqlSortTokenParser(List<SortToken> tokens)
+  {
+    _tokens = tokens;
+  }
+  public string Parse()
+  {
+    var orderByClause = new StringBuilder();
+    ParseExpression(orderByClause);
+    return orderByClause.ToString();
+  }
+
+  private void ParseExpression(StringBuilder sb)
+  {
+    for (int i = 0; i < _tokens.Count; i++)
+    {
+      sb.Append(RenderToken(_tokens[i]));
+      if (i < _tokens.Count - 1)
+        sb.Append(", ");
+    }
+  }
+
+  public string RenderToken(SortToken token)
+  {
+    if (token.Direction == SortDirection.Ascending)
+      return $"{token.Column.ToLower()}";
+    else
+      return $"{token.Column.ToLower()} desc";
+  }
+}
+
+public class MsSortTokenParser
+{
+  private List<SortToken> _tokens;
+  public MsSortTokenParser(List<SortToken> tokens)
+  {
+    _tokens = tokens;
+  }
+  public string Parse()
+  {
+    var orderByClause = new StringBuilder();
+    ParseExpression(orderByClause);
+    return orderByClause.ToString();
+  }
+
+  private void ParseExpression(StringBuilder sb)
+  {
+    for (int i = 0; i < _tokens.Count; i++)
+    {
+      sb.Append(RenderToken(_tokens[i]));
+      if (i < _tokens.Count - 1)
+        sb.Append(", ");
+    }
+  }
+
+  public string RenderToken(SortToken token)
+  {
+    if (token.Direction == SortDirection.Ascending)
+      return $"[{token.Column}]";
+    else
+      return $"[{token.Column}] DESC";
+  }
+}
