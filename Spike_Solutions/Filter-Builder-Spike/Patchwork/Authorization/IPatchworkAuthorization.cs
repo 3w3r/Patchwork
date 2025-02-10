fc98056fc26d387c74c83ee842061093ab862b61
Mark Ewer <MEwer@paylocity.com>
2025-02-10 09:44:09 -0600

    Added permission interface and default implementation (#12)

---
+++ Spike_Solutions/Filter-Builder-Spike/Patchwork/Authorization/IPatchworkAuthorization.cs
@@ -0,0 +1,11 @@
+﻿using System.Security;
+using System.Security.Principal;
+
+namespace Patchwork.Authorization;
+
+public interface IPatchworkAuthorization
+{
+  Permission GetPermission(string path, IPrincipal principal);
+  Permission GetPermissionToCollection(string schema, string entity, IPrincipal principal);
+  Permission GetPermissionToResource(string schema, string entity, string id, IPrincipal principal);
+}
