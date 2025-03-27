290fc6da87d9563340ef30f2b8581abe68c6eb8a
laiko <seth@3w3r.com>
2025-03-27 16:06:55 -0400

    Setup for Moq and preparing for Controller Tests

---
+++ Source-Code/Patchwork.Tests/Controllers/ControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseSchemaReader.Filters;
+using Moq;
+using Patchwork.Repository;
+
+namespace Patchwork.Tests.Controllers
+{
+
+    public class ControllerTests
+    {
+        ControllerTests()
+        {
+            var RepositoryMock = new Mock<IPatchworkRepository>();
+            RepositoryMock
+                .Setup(a => a.GetList(
+                    It.IsAny<string>(), //schemaName
+                    It.Is<string>(b => b == "DefaultLimitOffsetTest"), //entityName
+                    It.IsAny<string>(), //fields
+                    It.IsAny<string>(), //filter
+                    It.IsAny<string>(), //sort
+                    It.IsAny<int>(), //limit
+                    It.IsAny<int>() //offset
+                    ))
+                .Returns((
+                    string schemaName,
+                    string entityName,
+                    string fields,
+                    string filter,
+                    string sort,
+                    int limit,
+                    int offset
+                    ) =>
+                new GetListResult(
+                    new List<dynamic>(), 42, "LastId", limit, offset)
+                );
+        }
+
+        [Fact]
+        public void Does_GetListEndpoint_SupplyDefaultLimitAndOffset()
+        {
+            //Arrange
+
+            //Act
+            //Assert
+        }
+
+    }
+}
